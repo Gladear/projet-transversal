ee81f8ccef4a42f33bfeed542865aee7032e9fbd
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2019-12-10 22:14:22 +0100

    feat(simulator): add dotenv library

--- Simulator/src/main/java/me/gladear/simulator/App.java
+++ Simulator/src/main/java/me/gladear/simulator/App.java
@@ -1,7 +1,10 @@
 package me.gladear.simulator;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class App {
   public static final long TICK_TIME = 1000l;
+  public static final Dotenv dotenv = Dotenv.load();
 
   public static void main(final String[] args) throws Exception {
     // # 1. Starts a web socket connection
