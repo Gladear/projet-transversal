cdcf248535720d06a260c12b29f690e676e1aef2
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2019-12-16 10:48:00 +0100

    feat(server): add dotenv

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Create Flask application
 app = Flask(__name__)
 
+# Load application module
 import server.api
