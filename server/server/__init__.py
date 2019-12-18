a831fba2b3a4958b71eb08eedce55fdd994751d9
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2019-12-18 09:53:25 +0100

    chore(server): prepare flask-sockets

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sockets import Sockets
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -6,6 +7,15 @@ load_dotenv()
 
 # Create Flask application
 app = Flask(__name__)
+sockets = Sockets(app)
 
 # Load application module
 import server.api
+import server.ws
+
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
