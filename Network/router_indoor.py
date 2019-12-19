d33e533490c02caf8e03ba8e59f5d0736a44049f
pleu38 <gloutondu74@gmail.com>
2019-12-19 17:03:15 +0100

    Maj script

--- Network/router_indoor.py
+++ Network/router_indoor.py
@@ -13,27 +13,62 @@ file_loader = FileSystemLoader('.')
 #Inputs use with template j2
 hostname = input("Enter the hostname: ")
 port = input("Enter the connection port: ")
-nb_ro = input("Nombre d'interface :")
+nb_ro = input("Nombre d'interface: ")
 
 list_int = {}
 
 for i in range(int(nb_ro)) :
-    print("Configuration de linterface n°",i)
-    interface  = input("Number of interface :")
-    ip = input("Enter the @IP :")
-    subnet = input("Enter the subnet :")
-    list_int ["interface"] = interface
-    list_int ["ip"] = ip
+    print("Configuration de l'interface n°", i)
+    interface = input("Number of interface: ")
+    ip = input("Enter the @IP: ")
+    subnet = input("Enter the subnet: ")
+    list_int["interface"] = interface
+    list_int["ip"] = ip
     list_int["subnet"] = subnet
+
+print("Fin de configuration des interfaces")
+print("-----------------------------------\n")
+num_opsf = input("Enter the number of OSPF process: ")
+router_id = input("Enter the router-id: ")
+nb_ospf = input("Enter the number of OSPF route: ")
+
+#list_ospf = {}
+
+#for i in range(int(nb_ospf)) :
+ #   network_ospf = input("Enter the network for OSPF:")
+  #  area = input("Enter the area:")
+   # list_ospf["network"] = network_ospf
+    #list_ospf["area"] = area
+
+print("Fin de configuration de l'OSPF", num_opsf)
+print("-----------------------------------\n")
+
+# BGP configuration
+local_asn = input("Enter the local asn: ")
+nb_bgp = int(input("Enter the number of BGP neighbor: "))
+
+list_bgp = {}
+
+for i in range(nb_bgp):
+    bgp_neighbor = input("Enter the BGP neighbor: ")
+    remote_asn = input("Enter the remote asn: ")
+    sp_bgp_temp = bgp_neighbor.split(".")
+    sp_bgp = [sp_bgp_temp[0], sp_bgp_temp[1]]
+    if sp_bgp[0] == "192" and sp_bgp[1] == "168":
+        bgp_neighbor_loop = bgp_neighbor
+        loopback_temp = list_int["interface"]
 
 
 router = {
-    "address": "127.0.0.1"
+    "address": "127.0.0.1",
+    "port": "{{port}}"
 }
 
 # Load the environment
 #env = Environment(loader=file_loader)
 #template = env.get_template('bgp_template.j2')
+
 #output = template.render(local_asn='1111', bgp_neighbor='192.168.1.1', remote_asn='2222')
-#Print the output
-print(list_int)
+
+#Print the listbgp_template.j2
+print("loopback_temp : " + loopback_temp)
