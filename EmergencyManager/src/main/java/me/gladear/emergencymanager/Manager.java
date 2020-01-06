6d2104a9c325e9f7e51cb4f1b6be9b9f4a3f3180
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2020-01-06 15:00:47 +0100

    feat(EM): manage fire updates

--- EmergencyManager/src/main/java/me/gladear/emergencymanager/Manager.java
+++ EmergencyManager/src/main/java/me/gladear/emergencymanager/Manager.java
@@ -1,17 +1,31 @@
 package me.gladear.emergencymanager;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import me.gladear.emergencymanager.comm.WebSocketClientEndpoint;
+import me.gladear.emergencymanager.model.Geolocation;
+import me.gladear.emergencymanager.model.Sensor;
+import me.gladear.emergencymanager.model.Truck;
+import me.gladear.emergencymanager.utils.HttpUtils;
+import me.gladear.emergencymanager.utils.WSUtils;
 
 class Manager implements WebSocketClientEndpoint.MessageHandler {
     private final String ACTION_FIRE_UPDATE = "fire_update";
 
     private final WebSocketClientEndpoint client;
+    private final Map<Integer, Sensor> sensors;
 
     public Manager(WebSocketClientEndpoint client) {
         this.client = client;
+        this.sensors = new HashMap<>();
     }
 
     @Override
@@ -31,11 +45,91 @@ class Manager implements WebSocketClientEndpoint.MessageHandler {
     private void handleAction(String action, Object payload) {
         switch (action) {
             case ACTION_FIRE_UPDATE:
-                System.out.println(payload.toString());
+                this.manageFireUpdate((JSONObject) payload);
                 break;
             default:
                 System.out.println("Unknown action: " + action);
                 break;
         }
+    }
+
+    private void manageFireUpdate(JSONObject payload) {
+        try {
+            var id = payload.getInt("id");
+            var intensity = payload.getInt("intensity");
+
+            var sensor = this.sensors.get(id);
+            var exists = sensor != null;
+
+            if (!exists) {
+                var lat = payload.getDouble("lat");
+                var lon = payload.getDouble("lon");
+
+                var geoloc = new Geolocation(lat, lon);
+                sensor = new Sensor(id, geoloc);
+
+                this.sensors.put(id, sensor);
+            }
+
+            // Intensity may be used later to know
+            // if the sensor requires trucks to come
+            sensor.setIntensity(intensity);
+
+            if (intensity <= 0) {
+                // TODO The truck is available for some other fire
+                return;
+            }
+
+            if (exists) {
+                return;
+            }
+
+            // Get trucks from the server
+            var trucks = this.getTrucks();
+
+            // Filter available trucks
+            var available = trucks.stream()
+                .filter(truck -> truck.available)
+                .collect(Collectors.toList());
+
+            // If no truck is available,
+            // we'll wait for one to be,
+            // and handle the case above
+            if (available.isEmpty()) {
+                return;
+            }
+
+            // Compute which truck is to be sent
+            var sent = available.get(0);
+
+            // Send a message to the server telling them
+            // which truck to send
+            this.client.sendMessage(
+                WSUtils.createMessage("send_truck", sent.id)
+            );
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<Truck> getTrucks() throws IOException, JSONException {
+        var url = App.dotenv.get("SERVER_API") + "/trucks";
+        var data = HttpUtils.getJSONArray(url);
+
+        var trucks = new ArrayList<Truck>(data.length());
+
+        for (var item : data) {
+            var object = (JSONObject) item;
+
+            var id = object.getInt("id");
+            var lat = object.getDouble("lat");
+            var lon = object.getDouble("lon");
+            var available = object.getBoolean("available");
+
+            var truck = new Truck(id, new Geolocation(lat, lon), available);
+            trucks.add(truck);
+        }
+
+        return trucks;
     }
 }
